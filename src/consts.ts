03cf0f6a973403bb726e86fb0c754b2b00c91f7b
elvinzhu <zhuyongyu001@zhongan.com>
2021-12-28 21:05:18 +0800

    chore: Symbol fallback

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const hasSymbol = typeof Symbol === 'function';
-export const TYPE_KEY = hasSymbol ? Symbol('TYPE') : '__rfc_state_type';
+export const TYPE_KEY = hasSymbol ? Symbol('TYPE') : '__RFC_STATE_TYPE_KEY_DO_NOT_USE__';
 export const TYPE_TAKE_STATE = 'TAKE_STATE';
 export const TYPE_TAKE_PROPS = 'TAKE_PROPS';
