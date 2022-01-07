0c8af8cda3732d8e8ed363278fcdb7d1388041ee
elvinzhu <yongyu168@foxmail.com>
2022-01-08 00:53:27 +0800

    chore: remove file

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-// babel.config.js
-module.exports = (api) => {
-  // const isTest = api.env('test');
-  api.cache(true);
-  return {
-    presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-react', '@babel/preset-typescript'],
-  };
-};
